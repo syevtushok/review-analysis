b9bafda7f9a6ef8ec500edd8accfb11c0f8e8146
Serhii Yevtushok <ggazila@gmail.com>
2020-03-01 18:27:52 +0200

    basic configuration

---
+++ src/main/java/mate/academy/review/analysis/ReviewAnalysisApplication.java
@@ -0,0 +1,13 @@
+package mate.academy.review.analysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewAnalysisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReviewAnalysisApplication.class, args);
+    }
+
+}
